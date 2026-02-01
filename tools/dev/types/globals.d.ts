21d5b1ed9167a082228e7b6fda2e139e9ea89044
thoroc <thomas.a.roche@gmail.com>
2026-02-01 20:31:38 +0000

    fix: update globals.d.ts to ignore linting for noNamespace rule

--- tools/dev/types/globals.d.ts
+++ tools/dev/types/globals.d.ts
@@ -1,5 +1,5 @@
 // Minimal process env shape used in some tools
-
+// biome-ignore lint/style/noNamespace: true
 declare namespace NodeJS {
   interface ProcessEnv {
     [key: string]: string | undefined;
